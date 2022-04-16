8fa74dc357c87c2e1ae66ffe5757563825f73246
ck-fm0211 <chikafumi.k@gmail.com>
2022-04-16 22:54:27 +0900

    Feature/add condition format rule 24unlimit (#6)
    * feat: add ConditionalFormatRule for Unlimited PromoCode
    
    * style: apply prettier
    
    * stype: modify const value to snake case
    
    * style: add column idx

--- getPromoCode.gs.js
+++ getPromoCode.gs.js
@@ -3,8 +3,17 @@
  */
 
 // スプレッドシート
-const sheet = SpreadsheetApp.getActive().getSheetByName('コード一覧');
-const configSheet = SpreadsheetApp.getActive().getSheetByName('設定');
+const SHEET = SpreadsheetApp.getActive().getSheetByName("コード一覧");
+const CONFIG_SHEET = SpreadsheetApp.getActive().getSheetByName("設定");
+
+// 各列番号
+const INSERT_DATE_COLUMN_IDX = 1; // 記入日
+const MAIL_RECEIVED_DATE_COLUMN_IDX = 2; // メール受信日
+const PROMO_CODE_COLUMN_IDX = 3; // プロモコード
+const PROMO_CODE_AMOUNT_COLUMN_IDX = 4; // 容量
+const NUMBER_OF_TIMES_COLUMN_IDX = 5; // 使用回数
+const LIMIT_DATE_COLUMN_IDX = 6; // 利用期限
+const USED_FLAG_COLUMN_IDX = 7; // 使用済みフラグ
 
 /**
  * 本体
@@ -17,29 +26,28 @@ const configSheet = SpreadsheetApp.getActive().getSheetByName('設定');
  * 4. 処理したメールに処理済みラベルを付与
  */
 function getPromotionCodes() {
-
   console.log("[START]メール取得");
   // 検索条件に該当するスレッド一覧を取得
-  let threads = GmailApp.search('label:00_ギガ活  -label:00_ギガ活/処理済み');
+  let threads = GmailApp.search("label:00_ギガ活  -label:00_ギガ活/処理済み");
 
   console.log("[START]スレッド処理");
   // スレッドを一つずつ取り出す
-  threads.forEach(function(thread) {
+  threads.forEach(function (thread) {
     // スレッド内のメール一覧を取得
     let messages = thread.getMessages();
 
     // メールを一つずつ取り出す
-    messages.forEach(function(message) {
+    messages.forEach(function (message) {
       // メールからコード値と利用期限を抽出する
-      let values = extractCode(message)
+      let values = extractCode(message);
       // メールを処理しているのにプロモコードが取れなかったらアラート
-      if(values["codes"].length === 0){
+      if (values["codes"].length === 0) {
         console.log("[ERROR]データ抽出");
-        throw new Error("プロモコードの抽出に失敗しました")
+        throw new Error("プロモコードの抽出に失敗しました");
       }
       // スプレッドシートに書き出す
       console.log("[START]スプレッドシート書き込み");
-      writeSheet(values)
+      writeSheet(values);
     });
 
     // 重複を排除する
@@ -52,7 +60,7 @@ function getPromotionCodes() {
 
     // スレッドに処理済みラベルを付ける
     console.log("[START]ラベル追加");
-    let label = GmailApp.getUserLabelByName('00_ギガ活/処理済み');
+    let label = GmailApp.getUserLabelByName("00_ギガ活/処理済み");
     thread.addLabel(label);
   });
 }
@@ -62,20 +70,26 @@ function getPromotionCodes() {
  * 抽出する際、プロモコードは<strong>タグの中にあるため、html形式のbodyから正規表現で抽出する
  * 入力期限は「コードの入力期限」というメッセージのあとに記載されているため、プレーンテキストのメールbodyから抽出する
  */
-function extractCode(message){
+function extractCode(message) {
   let body = message.getBody();
   let plainBody = message.getPlainBody();
 
   let ret = {};
 
   // 形式:300MBXXXXXXXXX(300MBコード) or U24H10TXXXXXXXXX（24H使い放題10回分）
-  let codes = body.match(/([1-9]{1}[A-Z0-9]{13}|U24H[0-9]{1,2}T[A-Z0-9]{9})(?=.*<\/strong>)/g);
+  let codes = body.match(
+    /([1-9]{1}[A-Z0-9]{13}|U24H[0-9]{1,2}T[A-Z0-9]{9})(?=.*<\/strong>)/g
+  );
   ret["codes"] = codes;
   // 入力期限
-  let limitDate = plainBody.match(/(?<=コードの入力期限\n*)20[0-9]{2}\/[0-9]{2}\/[0-9]{2}/g);
+  let limitDate = plainBody.match(
+    /(?<=コードの入力期限\n*)20[0-9]{2}\/[0-9]{2}\/[0-9]{2}/g
+  );
   ret["limitDate"] = limitDate;
   //使用回数制限
-  let limitNumberOfTimes = plainBody.match(/(?<=コードの利用回数\n*)[0-9]{1,9}(?=回)/g);
+  let limitNumberOfTimes = plainBody.match(
+    /(?<=コードの利用回数\n*)[0-9]{1,9}(?=回)/g
+  );
   // nullのときは1、そうでないときはmatchした値
   ret["limitNumberOfTimes"] = !limitNumberOfTimes ? ["1"] : limitNumberOfTimes;
   // 受信日
@@ -87,25 +101,33 @@ function extractCode(message){
 /**
  * スプレッドシートに書き込む
  */
-function writeSheet(values){
+function writeSheet(values) {
   // 処理日
-  const today = new Date();
+  const TODAY = new Date();
 
   // 最終行を取得
-  let lastRow = sheet.getLastRow() + 1;
+  let lastRow = SHEET.getLastRow() + 1;
 
   // セルを取得して値を転記
   // code["limitNumberOfTimes"]の回数だけ繰り返す。
-  for(let i = 1; i <= Number(values["limitNumberOfTimes"]); i++){
-    let index = 0
-    for(let code of values["codes"]){
-      sheet.getRange(lastRow + index, 1).setValue(today);                  // 記入日
-      sheet.getRange(lastRow + index, 2).setValue(values["receiveDate"]);  // メール受信日
-      sheet.getRange(lastRow + index, 3).setValue(code);                   // プロモコード
-      sheet.getRange(lastRow + index, 4).setValue(getCodeAmount(code));    // 容量
-      sheet.getRange(lastRow + index, 5).setValue(i + "回目");              // 使用回数
-      sheet.getRange(lastRow + index, 6).setValue(values["limitDate"]);    // 使用期限
-      sheet.getRange(lastRow + index, 7).insertCheckboxes();               // 使用済みチェックボックス
+  for (let i = 1; i <= Number(values["limitNumberOfTimes"]); i++) {
+    let index = 0;
+    for (let code of values["codes"]) {
+      SHEET.getRange(lastRow + index, INSERT_DATE_COLUMN_IDX).setValue(TODAY); // 記入日
+      SHEET.getRange(lastRow + index, MAIL_RECEIVED_DATE_COLUMN_IDX).setValue(
+        values["receiveDate"]
+      ); // メール受信日
+      SHEET.getRange(lastRow + index, PROMO_CODE_COLUMN_IDX).setValue(code); // プロモコード
+      SHEET.getRange(lastRow + index, PROMO_CODE_AMOUNT_COLUMN_IDX).setValue(
+        getCodeAmount(code)
+      ); // 容量
+      SHEET.getRange(lastRow + index, NUMBER_OF_TIMES_COLUMN_IDX).setValue(
+        i + "回目"
+      ); // 使用回数
+      SHEET.getRange(lastRow + index, LIMIT_DATE_COLUMN_IDX).setValue(
+        values["limitDate"]
+      ); // 使用期限
+      SHEET.getRange(lastRow + index, USED_FLAG_COLUMN_IDX).insertCheckboxes(); // 使用済みチェックボックス
       index += 1;
     }
     lastRow += 1;
@@ -116,22 +138,54 @@ function writeSheet(values){
  * スプレッドシートに条件付き書式を設定する
  * 条件：使用済み列（F列）がTRUE（チェックON） 書式: グレーアウト
  */
-function upsertConditionalFormatRule(){
-  let conditionalFormatRule = SpreadsheetApp.newConditionalFormatRule()
-  .whenFormulaSatisfied('=$G1=TRUE')
-  .setBackground('#B7B7B7')
-  .setRanges([sheet.getRange(1,1,sheet.getLastRow() + 1, 7)])
-  .build()
-  let conditionalFormatRules = sheet.getConditionalFormatRules();
-  if(conditionalFormatRules.length > 0){
-    // 既存の条件付き書式と差し替え
-    conditionalFormatRules.splice(conditionalFormatRules.length - 1, 1, conditionalFormatRule);
-  }else{
-    // 初回起動時には条件付き書式がないので直接push
-    conditionalFormatRules.push(conditionalFormatRule)
-  }
+function upsertConditionalFormatRule() {
+  let conditionalFormatRuleUsedCodes = getConditionalFormatRuleUsedCodes();
+  let conditionalFormatRuleUnlimitedPromoCodes =
+    getConditionalFormatRuleUnlimitedPromoCodes();
+  let conditionalFormatRules = [
+    conditionalFormatRuleUsedCodes,
+    conditionalFormatRuleUnlimitedPromoCodes,
+  ];
+  // let conditionalFormatRules = sheet.getConditionalFormatRules();
+  // if(conditionalFormatRules.length > 0){
+  //   // 既存の条件付き書式と差し替え
+  //   conditionalFormatRules.splice(conditionalFormatRules.length - 1, 1, conditionalFormatRule);
+  // }else{
+  //   // 初回起動時には条件付き書式がないので直接push
+  //   conditionalFormatRules.push(conditionalFormatRule)
+  // }
   // 条件付き書式を設定
-  sheet.setConditionalFormatRules(conditionalFormatRules);
+  SHEET.setConditionalFormatRules(conditionalFormatRules);
+}
+
+/**
+ * 条件付き書式オブジェクトを返却する
+ * 条件：使用済み列（F列）がTRUE（チェックON） 書式: グレーアウト
+ */
+function getConditionalFormatRuleUsedCodes() {
+  let conditionalFormatRule = SpreadsheetApp.newConditionalFormatRule()
+    .whenFormulaSatisfied("=$G1=TRUE")
+    .setBackground("#474A4D")
+    .setRanges([
+      SHEET.getRange(1, 1, SHEET.getLastRow() + 1, SHEET.getLastColumn() + 1),
+    ])
+    .build();
+  return conditionalFormatRule;
+}
+
+/**
+ * 条件付き書式オブジェクトを返却する
+ * 条件：容量列（D列）に「無制限」の文字がある 書式: 背景色を薄いグレー
+ */
+function getConditionalFormatRuleUnlimitedPromoCodes() {
+  let conditionalFormatRule = SpreadsheetApp.newConditionalFormatRule()
+    .whenFormulaSatisfied('=find("無制限",$D1)')
+    .setBackground("#DCDDDD")
+    .setRanges([
+      SHEET.getRange(1, 1, SHEET.getLastRow() + 1, SHEET.getLastColumn() + 1),
+    ])
+    .build();
+  return conditionalFormatRule;
 }
 
 /**
@@ -139,10 +193,21 @@ function upsertConditionalFormatRule(){
  * 受信メールがスレッドにまとめられてしまった場合にデータ重複することがある。
  * 記入日列（A列）を除いて重複を排除する。
  */
-function removeDupilicatedRecords(){
-  let range = sheet.getRange(1,1,sheet.getLastRow() + 1, 7);
+function removeDupilicatedRecords() {
+  let range = SHEET.getRange(
+    1,
+    1,
+    SHEET.getLastRow() + 1,
+    SHEET.getLastColumn() + 1
+  );
 
-  range.removeDuplicates([2,3,4,5,7]);
+  range.removeDuplicates([
+    MAIL_RECEIVED_DATE_COLUMN_IDX,
+    PROMO_CODE_COLUMN_IDX,
+    PROMO_CODE_AMOUNT_COLUMN_IDX,
+    NUMBER_OF_TIMES_COLUMN_IDX,
+    USED_FLAG_COLUMN_IDX,
+  ]);
 }
 
 /**
@@ -154,27 +219,26 @@ function removeDupilicatedRecords(){
  *  3GB   -> 3GB/30Days
  *  20MB  -> 20GB/30Days
  */
-function getCodeAmount(code){
-
-  if(code.slice(0,5) == "300MB"){
-    return "300MB/3Days"
+function getCodeAmount(code) {
+  if (code.slice(0, 5) == "300MB") {
+    return "300MB/3Days";
   }
 
-  if(code.slice(0,3) == "U24"){
-    return "無制限/24H"
+  if (code.slice(0, 3) == "U24") {
+    return "無制限/24H";
   }
 
-  if(code.slice(0,3) == "1GB"){
-    return "1GB/7Days"
+  if (code.slice(0, 3) == "1GB") {
+    return "1GB/7Days";
   }
 
-  if(code.slice(0,3) == "3GB"){
-    return "3GB/30Days"
+  if (code.slice(0, 3) == "3GB") {
+    return "3GB/30Days";
   }
 
-  if(code.slice(0,4) == "20GB"){
-    return "20GB/30Days"
+  if (code.slice(0, 4) == "20GB") {
+    return "20GB/30Days";
   }
 
-  return "!!!!要確認!!!!"
+  return "!!!!要確認!!!!";
 }
--- notifyLimitDate.gs.js
+++ notifyLimitDate.gs.js
@@ -4,8 +4,8 @@
 
 // 処理日
 let dt = new Date();
-const limitDateCount = 7 // 7日以内
-dt.setDate(dt.getDate()+limitDateCount)
+const LIMIT_DATE_COUNT = 7; // 7日以内
+dt.setDate(dt.getDate() + LIMIT_DATE_COUNT);
 
 /**
  * スプレッドシートから指定の日付以内で期限が切れる未使用のプロモーションコードを抽出し、LINEに通知する
@@ -17,48 +17,62 @@ function notifyLimitDate() {
   let data = sheet.getRange(2, 1, lastRow - 1, lastColumn).getValues();
 
   let limitList = [];
-  data.some(function(value) {
+  data.some(function (value) {
     // 期限が指定の日付以内 かつ チェックボックスにチェックが付いていない
-    if((value[5].getTime()<=dt.getTime())&&(!value[6])){
-      limitList.push([Utilities.formatDate(value[5], "JST", "yyyy/MM/dd"), value[2]])
+    if (
+      value[LIMIT_DATE_COLUMN_IDX - 1].getTime() <= dt.getTime() &&
+      !value[USED_FLAG_COLUMN_IDX - 1]
+    ) {
+      limitList.push([
+        Utilities.formatDate(
+          value[LIMIT_DATE_COLUMN_IDX - 1],
+          "JST",
+          "yyyy/MM/dd"
+        ),
+        value[PROMO_CODE_COLUMN_IDX - 1],
+      ]);
     }
   });
   // 期限が近いものがある場合はLINEに通知する
-  if(limitList.length > 0){
+  if (limitList.length > 0) {
     console.log("[START]LINE通知");
     sendLINE(limitList);
-  }else{
+  } else {
     console.log("対象のプロモコードはありません");
-  };
+  }
 }
 
 /**
  * 受け取ったリストをもとにLINEに通知する
  */
 function sendLINE(limitList) {
-
   let limitMessageTemp = [];
-  for(let i = 0; i < limitList.length; i++) {
+  for (let i = 0; i < limitList.length; i++) {
     limitMessageTemp.push(limitList[i].join("："));
   }
 
-  let messageText = "\n" + String.fromCodePoint("0x26A0") + "有効期限が" + limitDateCount + "日以内のものがあります"
-  messageText += "\n\n"
-  messageText += "■期限日：対象コード"
-  messageText += limitMessageTemp.join("\n")
+  let messageText =
+    "\n" +
+    String.fromCodePoint("0x26A0") +
+    "有効期限が" +
+    LIMIT_DATE_COUNT +
+    "日以内のものがあります";
+  messageText += "\n\n";
+  messageText += "■期限日：対象コード";
+  messageText += limitMessageTemp.join("\n");
 
   // LINEから取得したトークン
-  let token = configSheet.getRange(1, 2).getValue()
+  let token = CONFIG_SHEET.getRange(1, 2).getValue();
   let options = {
-    "method" : "post",
-    "headers" : {
-      "Authorization" : "Bearer "+ token
+    method: "post",
+    headers: {
+      Authorization: "Bearer " + token,
     },
-    "payload" : {
-      "message" : messageText
-    }
-  }
+    payload: {
+      message: messageText,
+    },
+  };
 
-  let url  = "https://notify-api.line.me/api/notify"
-  UrlFetchApp.fetch(url, options)
+  let url = "https://notify-api.line.me/api/notify";
+  UrlFetchApp.fetch(url, options);
 }
